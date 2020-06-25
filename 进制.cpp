84c56797bcf55c02708014e7b877760bd0f115a4
UglyNoob <1253328221@qq.com>
2020-06-25 15:22:17 +0000

    Aha

--- 进制.cpp
+++ 进制.cpp
@@ -4,19 +4,36 @@
 int p,q,r,B;
 char a;
 
-bool detect(){
+bool detect(int p,int q,int r){
 	int P=0,Q=0,R=0,time=0;
-	while(){
-		P+=(p%10)*pow(B,time);
+	while(p){
+		if(p%10>=B)return false;
+		P+=(p%10)*(int)pow(B,time);
 		p/=10;
 		time++;
 	}
+	time=0;
+	while(q){
+		if(q%10>=B)return false;
+		Q+=(q%10)*(int)pow(B,time);
+		q/=10;
+		time++;
+	}
+	time=0;
+	while(r){
+		if(r%10>=B)return false;
+		R+=(r%10)*(int)pow(B,time);
+		r/=10;
+		time++;
+	}
+	if(P*Q==R)return true;
+	return false;
 }
 
 int main(){
 	scanf("%d %d %d",&p,&q,&r);
 	for(B=2;B<=16;B++){
-		if(detect()){
+		if(detect(p,q,r)){
 			printf("%d\n",B);
 			return 0;
 		}
