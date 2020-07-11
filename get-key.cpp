756aab370635f4d8eef7caba96e3c84ccdb91743
UglyNoob <1253328221@qq.com>
2020-07-11 05:18:42 +0000

    Aha

--- get-key.cpp
+++ get-key.cpp
@@ -1,7 +1,15 @@
 #include<cstdio>
 #include<conio.h>
+#include<cstring>
 
-int main(){
+int main(int argc,char **argv){
+	if(argc!=1){
+		if(!strcmp(argv[1],"true")){
+			for(int i=32;i<128;i++){
+				printf("%d:\t%c\n",i,(char)i);
+			}
+		}
+	}
 	while(true){
 		char c=getch();
 		printf("%d:\t%c\n",c,32<c&&127>c?c:' ');
