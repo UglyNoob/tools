16122d4bb0e43aa63f13ba67f15cecc145927c1f
UglyNoob <1253328221@qq.com>
2020-07-28 22:30:11 +0800

    Aha

--- RGB.py
+++ RGB.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 import pygame
 pygame.init()
-screen=pygame.display.set_mode((600,600))
+screen=pygame.display.set_mode(flags=pygame.FULLSCREEN)
 pygame.display.set_caption('RGB熬')
 font=pygame.font.SysFont(pygame.font.get_default_font(),48)
 r=0
 g=0
 b=0
 now=0
+w=screen.get_width()/2
+h=screen.get_height()/2
 while True:
     screen.fill((r,g,b))
     text=font.render('the United States of America',True,(255-r,255-g,255-b))
-    screen.blit(text,(299-text.get_width()/2,299-text.get_height()/2))
+    screen.blit(text,(w-text.get_width()/2,h-text.get_height()/2))
     pygame.display.update()
     if now==0:
         r+=1
--- count.py
+++ count.py
@@ -1,8 +1,34 @@
-import pygame
-pygane.init
+import pygame,time
+pygame.init()
 screen=pygame.display.set_mode(flags=pygame.FULLSCREEN)
-font=pygame.F
+font=pygame.font.SysFont(pygame.font.get_default_font(),160)
 hour=0
 minute=0
 second=0
-while
+w=screen.get_width()/2
+h=screen.get_height()/2
+screen.fill((0,135,255))
+pygame.display.update()
+while True:
+    text="%d:"%(hour)
+    if minute<10:text=text+"0"
+    text="%s%d:"%(text,minute)
+    if second<10:text=text+"0"
+    text="%s%d"%(text,second)
+    screen.fill((0,135,255))
+    text=font.render(text,True,(0,0,0))
+    screen.blit(text,(w-text.get_width()/2,h-text.get_height()/2))
+    pygame.display.update()
+    time.sleep(1)
+    second+=1
+    if second==60:
+        minute+=1
+        second=0
+        if minute==60:
+            minute=0
+            hour+=1
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
