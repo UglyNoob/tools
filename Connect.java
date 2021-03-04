c5e860265d8a7ffb3b174bc0a76abb6ad5399a31
UglyNoob <1253328221@qq.com>
2021-03-04 22:21:46 +0800

    Aha

--- Connect.java
+++ Connect.java
@@ -1,9 +1,15 @@
 import java.net.*;
+import java.io.*;
+
 public class Connect{
-	public static void main(String[] args){
-		if(args.length!=2)System.exit(1);
-		try{
-			new Socket(args[0],Integer.parseInt(args[1]));
-		}catch(Exception e){e.printStackTrace();}
+	public static void main(String[] args) throws Exception{
+		if(args.length!=2) {
+			System.exit(1);
+		}
+		Socket s = new Socket(args[0],Integer.parseInt(args[1]));
+		OutputStream os = s.getOutputStream();
+		while(true) {
+			os.write(System.in.read());
+		}
 	}
 }
