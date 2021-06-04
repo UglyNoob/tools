6c3fb8240c807a32ffd56c05b70795b49aa3f640
UglyNoob <1253328221@qq.com>
2021-06-05 07:30:25 +0800

    Aha

--- get-key.cpp
+++ get-key.cpp
@@ -3,15 +3,15 @@
 #include<cstring>
 
 int main(int argc,char **argv){
-	if(argc!=1){
-		if(!strcmp(argv[1],"true")){
-			for(int i=32;i<128;i++){
-				printf("%d:\t%c\n",i,(char)i);
+	if(argc != 1){
+		if(!strcmp(argv[1], "true")){
+			for(int i=32; i < 128; i++){
+				printf("%d:\t%c\n", i, (char)i);
 			}
 		}
 	}
-	while(true){
-		char c=getch();
-		printf("%d:\t%c\n",c,32<c&&127>c?c:' ');
+	while(true) {
+		char c = getch();
+		printf("%d:\t%c\n", c, 32 < c && 127 > c ? c : ' ');
 	}
 }
